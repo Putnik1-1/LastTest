55cd157ea9101095f747ec3080d17024b511dcee
Valery Simbirev <valerkin2001@yandex.ru>
2023-04-09 08:07:00 +0300

    save4

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+﻿string[] array1 = new string[5] {"1234", "234", "hello", "world", "res", "mir"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
@@ -20,3 +20,5 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
